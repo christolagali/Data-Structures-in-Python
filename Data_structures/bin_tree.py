04fa97a06bbdf2d7c8b23b68cf04654b6ba41dfe
christo.lagali <christo.lagali@gmail.com>
2019-03-02 22:37:50 -0800

    Adding parent attribute to TreeNode class for delete strategy

--- Data_structures/bin_tree.py
+++ Data_structures/bin_tree.py
@@ -1,17 +1,21 @@
 
 class TreeNode:
 
-    def __init__(self,key,value,leftChild=None,rightChild=None):
+    def __init__(self,key,value,leftChild=None,rightChild=None,parent=None):
         self.key = key
         self.value = value
         self.leftChild = leftChild
         self.rightChild = rightChild
+        self.parent = parent
     
     def getValue(self):
         return self.value
     
     def getKey(self):
         return self.key
+    
+    def isLeaf(self):
+        return True if self.leftChild == None and self.rightChild == None else False
 
 
 
@@ -19,6 +23,7 @@ class binarySearchTree:
 
     def __init__(self):
         self.root = None
+        self.size = 0
     
     # put method to insert a new node based on key value pair
     def put(self,key,value):
@@ -26,7 +31,8 @@ class binarySearchTree:
         if self.root:
             return self._put(key,value,self.root)
         else:
-            self.root = TreeNode(key,value)
+            self.root = TreeNode(key,value,parent=self.root)
+            self.size += 1
     
     """ 
         helper function for put() which does the heavy lifting of
@@ -34,14 +40,16 @@ class binarySearchTree:
     """
     def _put(self,key,value,currentNode):
         
-        t = TreeNode(key,value)
+        # create the node when a loc is found to assign parent
+        # t = TreeNode(key,value)
         # if key is less than root then we go left
         if key < currentNode.key:
 
             if currentNode.leftChild:
-                return self._put(key,value,currentNode.leftChild)
+                return self._put(key,value,currentNode.leftChild,)
             else:
-                currentNode.leftChild = t
+                currentNode.leftChild = TreeNode(key,value,parent=currentNode)
+                self.size += 1
 
         # need to handle duplicate key insertion
         # Here we willl update the value in case of duplicate key insertion
@@ -55,9 +63,10 @@ class binarySearchTree:
             if currentNode.rightChild:
                 return self._put(key,value,currentNode.rightChild)
             else:
-                currentNode.rightChild = t
+                currentNode.rightChild = TreeNode(key,value,parent=currentNode)
+                self.size += 1
     
-    
+
     # get: method to get the value assigned to a particular key
     def get(self,key):
 
@@ -89,8 +98,40 @@ class binarySearchTree:
         else:
             return self._get(key,currentNode.rightChild)
     
+    
+    def delete(self,key):
+
+        if self.size > 1:
+            nodeToRemove = self._get(key,self.root)
+
+            if nodeToRemove:
+                self.remove(nodeToRemove)
+                self.size -=1
+            else:
+                raise KeyError('Key Not found!')
 
         
+        elif self.size == 1 and self.root.key == key:
+            self.root = None
+            self.size -=1
+        
+        else:
+            raise KeyError('Key not found!')
+
+        
+    def remove(self,currentNode):
+        
+        # node to delete is a leaf
+        if currentNode.isLeaf():
+            # delete the reference to this child in the parent
+            if currentNode == currentNode.parent.leftChild:
+                currentNode.parent.leftChild = None
+            else:
+                currentNode.parent.rightChild = None
+
+    
+    
+
 
 
 # Global method to print the tree in inorder fashion
@@ -122,7 +163,12 @@ t.put(24,24)
 t.put(14,27)
 
 t.put(100,100)
-    
+
+print('Size of tree is ' , t.size)
+
+
 inOrder(t.root)
 
-print(t.get(24))
+#print(t.get(24))
+
+t.delete(100)
