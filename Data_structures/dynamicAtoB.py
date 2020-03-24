cf9aadaaff0532023982c83b384d3ff4c21d3b31
christo.lagali <christo.lagali@gmail.com>
2020-03-23 22:21:28 -0700

    Added Code for Robot movement around the Obstackles

--- Data_structures/dynamicAtoB.py
+++ Data_structures/dynamicAtoB.py
@@ -22,5 +22,41 @@ def AtoB():
 
 print(AtoB())
 
-print(AtoB())
+
+def AtoBObstacles(tracemat):
+
+    m=n  = len(tracemat)
+    
+
+    mat = list([0]*n for i in range(m))
+
+    for i in range(m):
+
+        for j in range(n):
+
+            if i ==0 and j==0 and tracemat[i][j] != 1:
+                mat[i][j] = 1
+            
+            elif tracemat[i][j] == 1:
+                mat[i][j] =0
+            
+            elif i ==0 and tracemat[i][j] !=1:
+                mat[i][j] = mat[i][j-1]
+            
+            elif j == 0 and tracemat[i][j] !=1:
+                mat[i][j] = mat[i-1][j]
+            
+            else:
+                mat[i][j] = mat[i][j-1] + mat[i-1][j]
+    
+    return mat
+
+            
+
+tracemat = [[0,0,0],[0,1,0],[0,0,0]]
+tracemat = [[1,0],[0,0]]
+tracemat = [[0,1],[1,0]]
+AtoBObstacles(tracemat)
+            
+
 
